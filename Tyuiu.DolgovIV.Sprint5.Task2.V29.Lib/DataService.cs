16b34835c800fea98e96910d0a2ba8d0583f8883
Tyuiu.DolgovIV@outlook.com <Ilya2017.dolgov@yandex.ru>
2025-11-16 17:35:49 +0500

    Task2

--- Tyuiu.DolgovIV.Sprint5.Task2.V29.Lib/DataService.cs
+++ Tyuiu.DolgovIV.Sprint5.Task2.V29.Lib/DataService.cs
@@ -8,19 +8,57 @@ namespace Tyuiu.DolgovIV.Sprint5.Task2.V29.Lib
         public string SaveToFileTextData(int[,] matrix)
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.csv");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+
+            string str = "";
+
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
+                
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    if (matrix[i, j] % 2 != 0)
+                    if (j != matrix.GetLength(1) - 1)
                     {
-                        File.AppendAllText(path, $"{0}\n");
+                        if (matrix[i, j] % 2 != 0)
+                        {
+                            str += "0" + ";";
+                        }
+                        else
+                        {
+                            str += $"{matrix[i, j]}" + ";";
+                        }
                     }
                     else
                     {
-                        File.AppendAllText(path, $"{matrix[i, j]}\n");
+                        if (matrix[i, j] % 2 != 0)
+                        {
+                            str += "0";
+                        }
+                        else
+                        {
+                            str += $"{matrix[i, j]}";
+                        }
                     }
+
+                    
                 }
+                if (i != matrix.GetLength(0) - 1)
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+                str = "";
+
             }
             return path;
         }
