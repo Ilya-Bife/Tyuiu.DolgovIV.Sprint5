12a3ca1ecca7caa40c12e5e13b80cf19cfa498f1
Ilya-Bife <ilya2017.dolgov@yandex.ru>
2025-11-22 17:43:55 +0500

    Task4

--- Tyuiu.DolgovIV.Sprint5.Task4.V21.Lib/DataService.cs
+++ Tyuiu.DolgovIV.Sprint5.Task4.V21.Lib/DataService.cs
@@ -7,6 +7,8 @@ namespace Tyuiu.DolgovIV.Sprint5.Task4.V21.Lib
         public double LoadFromDataFile(string path)
         {
             string x = File.ReadAllText(path);
+            int l = 0;
+            x = x.Replace('.', ',');
             double res = Math.Pow(Convert.ToDouble(x), 3) * Math.Cos(Convert.ToDouble(x)) + 2 * Convert.ToDouble(x);
             return Math.Round(res,3);
         }
